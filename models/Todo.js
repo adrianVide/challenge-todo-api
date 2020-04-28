4cea0071f26446d55891d24d3be84079a455bfbd
Adrian Vide <adrian.vide.paris@gmail.com>
2020-04-28 18:53:33 +0200

    statusDone

---
+++ models/Todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let Todo = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Todo", Todo);
---
+++ routes/todoRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const TodoController = require('../todosController/todos');
+
+const router = express.Router();
+
+router.get('/todos', TodoController.getAllTodos);
+router.get('/todos/:id', TodoController.getTodo);
+router.post('/todos', TodoController.createTodo);
+// router.put('/todos/:id', TodoController.updateTodo);
+router.put('/todos/done/:id', TodoController.doneHandler);
+router.delete('/todos/:id', TodoController.deleteTodo);
+
+module.exports = router;
